a51d264fac996a30a1b84f7eb18e6c8bf2c87beb
Suntet <99017042+Suntet@users.noreply.github.com>
2023-01-19 13:04:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('hai diraa');
+
+let btnShow = document.querySelector('button');
+let result = document.querySelector('h1');
+
+btnShow.addEventListener('click', () => {
+    let checkbox = document.querySelector('input[type="checkbox"]:checked');
+    result.innerText = checkbox.parentElement.textContent;    
+});
